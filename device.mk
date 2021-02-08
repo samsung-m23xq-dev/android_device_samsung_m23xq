a22680fa4d37deed9d08fe9a548c04cb2e3cdce8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:37:15 +0100

    m23xq: Build fastbootd
    Change-Id: I7d23da657e7c6220c9cc3f80734884c204f5212a
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -7,8 +7,16 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/m23xq/m23xq-vendor.mk)
