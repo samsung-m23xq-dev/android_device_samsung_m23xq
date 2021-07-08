c054fceff1b05fd9caafeee1e17f754736e61ebf
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-08 16:04:36 +0200

    m23xq: Add power AIDL DT2W support via sysfs node
    Change-Id: Idcff758444a5c89ba20f2ae804a407ee78c445cb
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,9 @@ endif
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := lito
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
