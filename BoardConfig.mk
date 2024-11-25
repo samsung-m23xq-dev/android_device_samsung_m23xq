cf1fd774f864486e879f91cd5dd1566c8023ff5b
atharvnegi <atharvnegi2951@gmail.com>
2024-11-26 05:04:38 +0530

    m23xq: Add arch and bootloader flags
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,24 @@
 
 DEVICE_PATH := device/samsung/m23xq
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo570
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := kryo570
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := lito
+TARGET_NO_BOOTLOADER := true
+
 # Inherit the proprietary files
 include vendor/samsung/m23xq/BoardConfigVendor.mk
