6f15fc96a69e13dd5265f0b905c95187d76ef828
atharvnegi <atharvnegi2951@gmail.com>
2024-11-26 05:48:25 +0530

    m23xq: Build Bluetooth Stack
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -43,6 +43,12 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
