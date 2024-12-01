f4c1f29378fcb90056a5a53c55222ab155f50ead
atharvnegi <atharvnegi2951@gmail.com>
2024-12-01 21:20:11 +0530

    m23xq: Add The FCM
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,8 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_m23xq
