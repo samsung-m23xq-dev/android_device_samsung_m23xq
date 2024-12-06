b3b19cacfb76b7a47c6f0595ec535c4c0da04986
Simon1511 <simon2002.schoenmackers@gmail.com>
2024-12-06 07:42:48 +0530

    m23xq: Build gatekeeper
    *And use stock init file
    
    Change-Id: I1085816bbda0a4d9c9d369ae1b42a20aeb6fe11d
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -196,7 +196,11 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/init/android.hardware.gatekeeper@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.gatekeeper@1.0-service.rc
 
 # Gnss
 PRODUCT_PACKAGES += \
